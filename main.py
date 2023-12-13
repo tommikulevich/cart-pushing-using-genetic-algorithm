c1de85c55077adc4286b2f04cd90d232e78ddf87
Tomash Mikulevich <tommikulevich@gmail.com>
2023-12-13 17:03:09 +0100

    Add some features in saving and plotting data

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import os
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-N = 15
+N = 45
 POPULATION_SIZE = 70
-NON_ELITE_SIZE = int(0.90 * POPULATION_SIZE)
-GENERATIONS = 4000
+NON_ELITE_RATE = 0.90
+NON_ELITE_SIZE = int(NON_ELITE_RATE * POPULATION_SIZE)
+GENERATIONS = 1000
 MUTATION_RATE = 0.25
+
+LOGS_PATH = "Arch/logs"
+LOG_NAME = f"{LOGS_PATH}/data_N{N}_PS{POPULATION_SIZE}_NER{NON_ELITE_RATE}_G{GENERATIONS}_MR{MUTATION_RATE}.txt"
 
 
 class GeneticAlgorithm:
@@ -34,20 +39,14 @@ class GeneticAlgorithm:
         return x[N - 1, 1] - (1 / (2*N)) * np.sum(u ** 2)
 
     def evaluate_population(self):
-        for i in range(POPULATION_SIZE):
-            self.fitness[i] = self.evaluate(self.population[i])
-
-    def select_parents(self):
-        sorted_indices = np.argsort(self.fitness)
-        return self.population[sorted_indices[-1]], self.population[sorted_indices[-2]]
+        self.fitness = np.apply_along_axis(self.evaluate, 1, self.population)
 
     def wheel_selection(self):
         normalized_fitness = np.maximum(self.fitness, 0)
         total_fitness = np.sum(normalized_fitness)
         selection_probabilities = normalized_fitness / total_fitness
         selected_indices = np.random.choice(len(self.population), 2, p=selection_probabilities)
-        selected_individuals = [self.population[i] for i in selected_indices]
-        return selected_individuals
+        return self.population[selected_indices]
 
     @staticmethod
     def single_point_crossover(parent1, parent2):
@@ -72,8 +71,8 @@ class GeneticAlgorithm:
         self.evaluate_population()
         elite_population = self.elite()
         for i in range(NON_ELITE_SIZE):
-            parent1, parent2 = self.wheel_selection()
-            child = self.single_point_crossover(parent1, parent2)
+            parents = self.wheel_selection()
+            child = self.single_point_crossover(*parents)
             new_population[i] = self.mutate(child)
         self.population = np.concatenate((new_population, elite_population))
 
@@ -82,14 +81,26 @@ class GeneticAlgorithm:
             self.evolve()
             self.evaluate_population()
             self.best_fitness.append(np.max(self.fitness))
+        self.save_data_to_file()
 
     def plot_fitness_evolution(self):
+        best_fitness = self.calculate_ideal_fitness()
         plt.figure()
         plt.plot(self.best_fitness)
-        plt.title(f'Fitness Evolution ({self.best_fitness[-1]:.6f})')
+        plt.axhline(y=best_fitness, color='r', linestyle='--', label=f"J* = {best_fitness:.6f}")
+        plt.title(f'Fitness Evolution')
         plt.xlabel('Generation')
         plt.ylabel('Fitness')
+        plt.legend()
         plt.show()
+
+    def save_data_to_file(self):
+        os.makedirs(LOGS_PATH, exist_ok=True)
+        np.savetxt(LOG_NAME, self.best_fitness)
+
+    @staticmethod
+    def load_data_from_file(filename):
+        return np.loadtxt(filename)
 
 
 if __name__ == "__main__":
